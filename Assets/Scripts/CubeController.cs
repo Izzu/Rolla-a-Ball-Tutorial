23e411cd2e6114c9340b27cb9fea19890312981a
Andrew <colvinwandrew@gmail.com>
2016-02-25 08:27:18 -0600

    publish

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody rigid;
+    public float speed;
+
+    void Start()
+    {
+       // run = cube.transform.position;
+       rigid = GetComponent<Rigidbody>();
+       Debug.Log(rigid);
+    }
+
+    void FixedUpdate()
+    {
+        float distance = Vector3.Distance(player.transform.position, gameObject.transform.position);
+        //Vector3 move = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        //Vector3 move = new Vector3(player.GetComponent<Rigidbody>().velocity.x, 0, player.GetComponent<Rigidbody>().velocity.y);
+        //Debug.Log(move);
+
+        if (distance < 3f)
+        {
+            Vector3 move = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            rigid.AddForce(move.normalized / 2f);
+            //gameObject.GetComponent<Rigidbody>().velocity = player.GetComponent<Rigidbody>().velocity;
+            Debug.Log("distance < 3f");
+            //rigid.AddForce(move * speed);
+            //Debug.Log(gameObject.GetComponent<Rigidbody>().velocity);
+
+        }
+
+        Debug.Log(rigid);
+        
+    }
+}
